88d7d246e0e030d65b3b3397dc817d08ac23ce5f
Daniel Graña <dangra@gmail.com>
2011-08-31 12:06:03 -0300

    reduce max failures per task setting to a practical value of 3

--- master/src/config.hrl
+++ master/src/config.hrl
@@ -14,7 +14,7 @@
 
 % Default setting for the maximum number of failures
 % allowed per task
--define(TASK_MAX_FAILURES, 100).
+-define(TASK_MAX_FAILURES, 3).
 
 % A failed task is paused for
 % min(C * FAILED_MIN_PAUSE, FAILED_MAX_PAUSE) +
